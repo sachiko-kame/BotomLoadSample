55a9801bbbaaadaadb0652dbc324b362bdc9a51a
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2018-03-04 21:44:04 +0900

    インディケーターのcellの作成(仮)

---
+++ BotomLoadSample/Parts/LoaddingTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  LoaddingTableViewCell.swift
+//  BotomLoadSample
+//
+//  Created by 水野祥子 on 2018/03/04.
+//  Copyright © 2018年 sachiko. All rights reserved.
+//
+
+import UIKit
+
+class LoaddingTableViewCell: UITableViewCell {
+    @IBOutlet weak var IndicatoreView: UIActivityIndicatorView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+    func setCell(){
+//        IndicatoreView.startAnimating()
+//        IndicatoreView.stopAnimating()
+    }
+}
